ddffd1f9672670468396fd343a34b2ff8079c3f4
amckenzi <amckenzi@pratt.edu>
2016-04-28 13:34:15 +0000

    Last week

---
+++ ruby/example.rb
@@ -0,0 +1,6 @@
+def addNums(num1, num2)
+    sum = num1 + num2
+    puts sum
+end
+
+addNums(1,2)
--- truth/js/main.js
+++ truth/js/main.js
@@ -4,7 +4,7 @@ var addSide = "addTruth";
 
 $(".contentcell").css("width",$(window).width()+"px");
 
-$(window).scroll(function() {
+/*$(window).scroll(function() {
   
     console.log($(window).scrollLeft()/$(document).width());
   
@@ -32,9 +32,33 @@ $(window).scroll(function() {
   
 });
 
+*/
 
 $(".statement").click(function(){
     $(this).hide();
 
     
+});
+
+var bodyClick = 0
+
+$("body").mousedown(function() {
+    bodyClick = bodyClick + 1;
+  
+});
+
+
+//infinite scroll
+$(window).scroll(function() {
+  console.log($(window).scrollTop());
+  console.log($(window).height());
+  console.log($(document).height());
+  
+  if (($(window).width() + $(window).scrollLeft()) == $(document).width()) {
+    $(window).scrollLeft(10);
+  } else if ($(window).scrollLeft() === 0) {
+  $(window).scrollLeft($(document).width() - $(window).width() + 10); 
+  }
+  
+  
 });
